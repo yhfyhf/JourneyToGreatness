2af1465f15dad2c576ecd0578e2f8a39e37e4d3a
Wilbeibi <wilbeibi@gmail.com>
2014-10-22 01:17:13 -0400

    LetterCombinations

---
+++ LetterCombinations.py
@@ -0,0 +1,29 @@
+class Solution:
+    # @return a list of strings, [s1, s2]
+    def letterCombinations(self, digits):
+        tab = {'2':'abc', '3':'def','4':'ghi','5':'jkl','6':'mno','7':'pqrs','8':'tuv','9':'wxyz'}
+        res = []
+        self.dfs(tab, digits, res, "")
+        return res
+
+    def dfs(self, tab, digits, res, buff):
+        """
+        Args:
+        tab: convert table
+        digits: input string
+        res: result list
+        buff: buffer to store intermediate string
+        cnt: buff length count
+        """
+        if len(digits) == 0:
+            res.append(buff)
+            return
+
+        for c in tab[digits[0]]:
+            self.dfs(tab, digits[1:], res, buff+c)
+            
+
+        
+if __name__ == '__main__':
+    so = Solution()
+    print so.letterCombinations("23")
